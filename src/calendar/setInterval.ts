389683ff3fb5c8f3001acf4c78ae0326f324d4bb
Maria Cidoncha <mariacidoncha@gmail.com>
2024-02-01 00:02:54 +0100

    clean setInterval

--- src/calendar/setInterval.ts
+++ src/calendar/setInterval.ts
@@ -1,33 +1,29 @@
 import { getEventsFromLS } from '../globalFunctions.js'
 
-export function checkEventReminder() {
+export function checkEventReminder(): void {
 
-    const events = getEventsFromLS()
-
+    const events = getEventsFromLS();
     const filterEvents =  events.filter(event => !event.notificated);
-
     const notFilteredEvents = events.filter( event => event.notificated);
-
-    const returnedEvents = [...notFilteredEvents]
+    const returnedEvents = [...notFilteredEvents];
 
     filterEvents.forEach((event) => {
-        const startTime = new Date( event.initialDate )
+        const startTime = new Date( event.initialDate );
         const expirationTime = startTime.getTime();
         const actualTime = Date.now();
         const reminderTimeMilli =  Number(event.alertTime) * 60 * 1000;
         const timeLeft = expirationTime - actualTime;
-        const updatedEvent = {...event}
+        const updatedEvent = {...event};
+
         if (timeLeft <=  reminderTimeMilli && !event.notificated ) {
-            updatedEvent.notificated = true            
+            updatedEvent.notificated = true;        
             alert(`reminder: The event ${event.title} starts in less than ${event.alertTime} minutes!`);
         }
-        returnedEvents.push(updatedEvent)
+        returnedEvents.push(updatedEvent);
     });
 
     const LSData:string = localStorage.getItem('calendar')!;
-
     const currentMonth = JSON.parse(LSData).currentMonth;
-
     const newCalendar = JSON.stringify( { currentMonth,  eventList: returnedEvents });
 
     localStorage.setItem('calendar', newCalendar );
